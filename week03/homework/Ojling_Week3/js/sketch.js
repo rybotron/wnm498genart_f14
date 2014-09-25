9b4b7c172cc834fda2f9d9e6aaddaee67e7b6365
scarsam <samojling@Sams-MacBook-Pro.local>
2014-09-25 12:07:53 -0700

    Ojling Week 3 Homework

---
+++ week03/homework/Ojling_Week3/js/sketch.js
@@ -0,0 +1,31 @@
+var gui;
+var transforms = {
+  modifier: 0.1
+};
+
+function setup() {
+  myCanvas = createCanvas(windowWidth, windowHeight);
+  gui = new dat.GUI();
+  gui.add(transforms, "modifier", 0, 5);
+}
+ 
+function draw() 
+{
+  background(52, 54, 73);
+  fill(227, 111, 33); 
+  stroke(255);
+  ellipse( width / 2, height / 2, width / 4, width / 4 );
+  push();
+  translate( width / 2, height / 2);
+  noFill();
+  stroke(255);
+  rotate(millis() * transforms.modifier * -0.001);
+  ellipse( width / 5, height / 5, width / 15, width / 15 );
+  pop();
+}
+
+// Window event handler when the browser window size changes
+// When resized it calls the anonymous function
+window.onresize = function(){
+	myCanvas.size(windowWidth, windowHeight);
+}
