ca58ded8844e984cd0db9ecfec9070e2d28ec874
Hugo Baeta <hugo@baeta.me>
2014-10-09 11:57:07 -0700

    Homework week5 commit

---
+++ week05/homework/baeta_week5/js/sketch.js
@@ -0,0 +1,40 @@
+var amp,
+	theta = 0;
+
+function preload(){
+  background(200,220,230);
+  sound = loadSound('audio/fool.m4a');
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+	sound.play();
+
+	// amp = new p5.Amplitude(); // Measures overall volume between 0.0 and 1.0
+	amp = new p5.Amplitude(.8); // Adding a number between 0.0 and .999 will smooth the amplitude reading
+
+}
+
+function draw() {
+
+	background(230,240,250);
+	noStroke();
+	fill(0);
+
+	translate(windowWidth / 2, windowHeight / 2);
+	var level = amp.getLevel(); // Get the amplitude level in a 0.0 - 1.0 range then scaling it by ampScale
+
+	var diameter = map( level, 0, 1, 10, 500);
+
+	if (diameter < 200) {
+		fill(255-diameter/2,200,200)
+	} else {
+		fill(50,50,255-diameter/2)
+	}
+
+	ellipse( 0, 0, diameter*2, diameter*2 );
+
+
+
+}
